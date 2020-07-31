afc2adf496a3e7d6da00d0a8aa493bb4e55f3afa
Michał Wieczorek <michal126@hotmail.com>
2020-07-31 11:33:25 +0200

    changed structure added ef connection

---
+++ EntityFramework/GarageOrganizerDbContext.cs
@@ -0,0 +1,28 @@
+﻿using GarageOrganizer.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFramework
+{
+    public class GarageOrganizerDbContext: DbContext
+    {
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Car> Cars { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Client>()
+                .HasMany(c => c.Cars)
+                .WithOne(c => c.Client)
+                .IsRequired();
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=GarageOrganizerDb;Trusted_Connection=True;");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
