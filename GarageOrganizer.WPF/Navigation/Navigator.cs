77fa123d3867daccd1d33b30b8c23c8ecd196198
Michał Wieczorek <michal126@hotmail.com>
2020-08-04 18:35:17 +0200

    Implemeted INotifyPropertyChange to update ViewModel

--- GarageOrganizer.WPF/Navigation/Navigator.cs
+++ GarageOrganizer.WPF/Navigation/Navigator.cs
@@ -1,15 +1,36 @@
 ﻿using GarageOrganizer.WPF.Commands;
 using GarageOrganizer.WPF.ViewModels;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 using System.Windows.Input;
 
 namespace GarageOrganizer.WPF.Navigation
 {
-    public class Navigator: INavigator
+    public class Navigator: INavigator, INotifyPropertyChanged
     {
-        public BaseViewModel CurrentViewModel { get; set; }
+        private BaseViewModel _currentViewModel;
+        public BaseViewModel CurrentViewModel 
+        { 
+            get 
+            {
+                return _currentViewModel;
+            } 
+            set
+            {
+                _currentViewModel = value;
+                OnPropertyChanged(nameof(CurrentViewModel));
+            }
+        }
         public ICommand UpdateCurrentViewModelCommand => new UpdateCurrentViewModelCommand(this);
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
