6db35fbd1b3527c6b173adaa02c75c359bf9cf08
Michał Wieczorek <michal126@hotmail.com>
2020-08-11 14:28:05 +0200

    clientsview modification

---
+++ GarageOrganizer.WPF/Controls/ClientList.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GarageOrganizer.WPF.Controls
+{
+    /// <summary>
+    /// Interaction logic for ClientList.xaml
+    /// </summary>
+    public partial class ClientList : UserControl
+    {
+        public ClientList()
+        {
+            InitializeComponent();
+        }
+    }
+}
